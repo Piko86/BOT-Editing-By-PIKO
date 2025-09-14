e3cb4f117cd79c6c392a5b98ed83e61434715dae
Piko86 <manmithabackup@gmail.com>
2025-09-15 01:50:30 +0530

    Update group-mute.js

--- plugins/group-mute.js
+++ plugins/group-mute.js
@@ -1,5 +1,6 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
+const { isBotAdmin } = require('../lib/isAdmin');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
 cmd({
@@ -14,7 +15,7 @@ async (conn, mek, m, { from, isGroup, senderNumber, isAdmins, isBotAdmins, reply
     try {
         if (!isGroup) return reply("❌ This command can only be used in groups.");
         if (!isAdmins) return reply("❌ Only group admins can use this command.");
-        if (!isBotAdmins) return reply("❌ I need to be an admin to mute the group.");
+        if (!isBotAdmin) return reply("❌ I need to be an admin to mute the group.");
 
         await conn.groupSettingUpdate(from, "announcement");
         reply("✅ 𝐆ʀᴏᴜ𝐏 𝐇ᴀ𝐒 𝐁ᴇᴇ𝐍 𝐌ᴜᴛᴇ𝐃. 𝐎ɴʟ𝐘 𝐀ᴅᴍɪɴ𝐒 𝐂ᴀ𝐍 𝐒ᴇɴ𝐃 𝐌ᴇꜱꜱᴀɢᴇ𝐒.");
